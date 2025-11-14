762ca17d61714f6b2bf95cbd446a48d7c7f22a5e
zavfake <zavfake@gmail.com>
2025-11-14 21:07:48 +0700

    ADDED: New loops for

---
+++ 8-1-loops.py
@@ -0,0 +1,14 @@
+myList = list(range(0,11))
+print(myList)
+
+print("=====")
+for i in range(0, 11):
+    print(i)
+    
+print("==cara penulisan lain jika dimulai dari 0==")
+for i in range(11):
+    print(i)
+    
+print("== cara buat list tapi dengan lompatan n value ==")
+myList = list(range(0,11,3))
+print(myList)
